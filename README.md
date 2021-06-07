11bd281f43153f343fb56304f7ca6ebb53343566
Havoc3sevens <ehurtado9@gmail.com>
2021-06-06 23:11:32 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Q_Harmonic_Oscillator
+Solves the Quantum Harmonic Oscillartor wave functions.
---
+++ Source Code/main.cpp
@@ -0,0 +1,16 @@
+/*
+This program solves the equations for the quantum harmonic oscillator
+    Created by: Enrique Hurtado
+    Date: 06 June 2021
+    Latest update: 06 June 2021
+    History:
+        Date: 06/06/21 || Mod: Program written || By: Enrique Hurtado
+    Purpose: N/A
+    Notes: N/A
+*/
+#include <iostream>
+
+int main()
+{
+    
+}
