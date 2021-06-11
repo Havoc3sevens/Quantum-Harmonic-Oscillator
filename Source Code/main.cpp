72b6e715e26c795409514e05b764b8bda5f2c32e
Havoc3sevens <ehurtado9@gmail.com>
2021-06-11 00:16:02 -0700

    Updated main.cpp preamble

--- Source Code/main.cpp
+++ Source Code/main.cpp
@@ -5,6 +5,7 @@ This program solves the equations for the quantum harmonic oscillator
     Latest update: 06 June 2021
     History:
         Date: 06/06/21 || Mod: Program written || By: Enrique Hurtado
+        Date: 06/11/21 || Mod: Added psi_n_to_file || By: Enrique Hurtado
     Purpose: N/A
     Notes: N/A
 */
